1b9a3389873ce96d60bb7d12eb9be83aceb8ef4c
Valentim Affonso <timzejr2@gmail.com>
2022-06-07 12:06:20 -0300

    Implementado verificação de Xeque

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -15,6 +15,11 @@ namespace xadrez_console
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
             Console.WriteLine("Aguardando Jogada: " + partida.jogadorAtual);
+            if (partida.xeque)
+            {
+                Console.WriteLine("Xeque!");
+                Console.ReadLine();
+            }
         }
 
         public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
